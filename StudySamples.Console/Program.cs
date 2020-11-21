957c9a9afb314ec44b4ed3d5989b817c30b927d3
Vitalii Martyniuk <vitaliy.martinyk@gmail.com>
2020-11-21 20:07:58 +0200

    Initial commit

---
+++ StudySamples.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudySamples.ConsoleUi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
